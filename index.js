51953474ea3ca3ab6775740770c39d6daaaea1c7
Juan Miguel Guerrero <juanmiguel@existo.es>
2019-12-22 12:40:01 +0100

    Servidor como middleware express

--- index.js
+++ index.js
@@ -1,13 +1,46 @@
-const { ApolloServer, gql } = require('apollo-server')
+const express = require('express')
+const server = express()
+const cors = require('cors')
+const morgan = require('morgan')
+const port = process.env.PORT || 8080
+
+const { ApolloServer, gql } = require('apollo-server-express')
 
 const schema = require('./schema/generateSchema.js')
+const resolvers = require('./resolvers/generateResolvers.js')
 const typeDefs = gql(schema)
 
-// const resolvers = require('./resolvers/resolvers.js')
-const resolvers = require('./resolvers/generateResolvers.js')
+const apollo = new ApolloServer({ typeDefs, resolvers })
+const api = require('./api')
 
-const server = new ApolloServer({ typeDefs, resolvers })
+server.use(express.json())
+server.use(express.urlencoded({extended: true}))
+server.use(morgan('tiny'))
+server.use(cors())
 
-server.listen().then(({ url }) => {
-  console.log(`🚀  Server ready at ${url}`);
+apollo.applyMiddleware({
+	app: server,
+	path: '/graphql',
 });
+
+server.use("/", api)
+
+// catch 404 and forward to error handler
+server.use(function(req, res, next) {
+	res.status(404)
+	const error = new Error('Not Found')
+	next(error)
+});
+
+// error handler
+server.use(function(error, req, res, next) {
+  res.status(res.statusCode || 500);
+  res.json({
+	  message: error.message
+  })
+});
+
+// Deploy server
+server.listen(port, () => {
+	console.log(`Server running on port: ${port} ${apollo.graphqlPath}`)
+})
