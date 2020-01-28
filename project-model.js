21be36eb935a27c9f8bed1fcff7d103ab68996c5
Juan Miguel Guerrero <juanmiguel@existo.es>
2020-01-28 09:58:04 +0100

    Actualización modelo de datos y schema generado

--- project-model.js
+++ project-model.js
@@ -58,8 +58,16 @@ module.exports =
 
 // Schema GraphQL a generar
 
+
+// Generar los tipos de las colecciones
+// Un tipo por cada colección
+
+// TODO: Comprobar qué pasa marcando los obligatorios
+
 type Book {
 	id: ID
+	created: Date
+	update: Date
 	title: String
 	author: String
 	price: Float
@@ -67,69 +75,117 @@ type Book {
 
 type Author {
 	id: ID
+	created: Date
+	update: Date	
 	name: String
 	age: Int
 }
+
+// Generar las Query por colección
+// Para cada colección generamos la query Item y allItems
 
 type Query {
 	Book(id: ID!) : Book
 	allBooks(
 		page: Int, 
 		perPage: Int, 
-		sortField: String, 
-		sortOrder: String,
-		filter: BookFilter) : [Book]
+		orderBy: [BookOrderBy], 
+		filter(BookFilter) : [Book]
 	
 	Author(id: ID!) : Author
 	allAuthors(
 		page: Int, 
 		perPage: Int, 
-		sortField: String, 
-		sortOrder: String,
-		filter: AuthorFilter) : [Author]
-}
-
-type Mutation {
-	createBook(data: BookInput) : Book
-	updateBook(id: ID!, data: BookInput) : Book
-	deleteBook(id: ID!) : Boolean
-
-	createAuthor(data: AuthorInput) : Author
-	uptdateAuthor(id: ID!, data: AuthorInput) : Author
-	deleteAuthor(id: ID!) : Boolean
-}
-
-input BookInput {
-	title: String!
-	author: String!
-	price: Float!
+		orderBy: [AuthorOrderBy], 
+		filter(AuthorFilter) : [Author]
 }
 
 input BookFilter {
 	q: String
 	id: ID
+	created: DateFilter
+	update: DateFilter	
 	title: StringFilter
 	author: StringFilter
 	price: FloatFilter
 }
 
-input AuthorInput {
-	name: String!
-	age: Int
+enum BookOrderBy {
+	title_ASC
+	title_DESC
+	author_ASC
+	author_DESC
+	price_ASC
+	price_DESC
 }
 
 input AuthorFilter {
 	q: String
 	id: ID
+	created: DateFilter
+	update: DateFilter		
 	name: StringFilter
 	age: IntFilter
 }
 
+enum AuthorOrderBy {
+	name_ASC
+	name_DESC
+	age_ASC
+	age_DESC
+}
+
+// Generar las mutaciones
+// Por cada colección generamos create, update y delete
+
+type Mutation {
+	createBook(data: BookCreateInput) : Book
+	updateBook(id: ID!, data: BookUpdateInput) : Book
+	deleteBook(id: ID!) : Book
+
+	createAuthor(data: AuthorCreateInput) : Author
+	uptdateAuthor(id: ID!, data: AuthorUpdateInput) : Author
+	deleteAuthor(id: ID!) : Author
+}
+
+// Generar los inputs
+// Por cada colección generamos el input del Item y el Filter
+
+input BookCreateInput {
+	title: String!
+	author: String!
+	price: Float!
+}
+
+input BookUpdateInput {
+	title: String
+	author: String
+	price: Float
+}
+
+input AuthorCreateInput {
+	name: String!
+	age: Int
+}
+
+input AuthorUpdateInput {
+	name: String
+	age: Int
+}
+
+// Añadir los tipos de datos al schema
+// Partimos de 4 tipos String, Float, Int, Date y Boolean
+
 type StringFilter {
 	eq: String
 	neq: String
-	in: [String]
-	notIn: [String]
+	matches: StringMatchesFilter
+}
+
+type StringMatchesFilter {
+	pattern: String!
+	caseSensitive: Boolean = false
+	regexp: Boolean = false
 }
 
 type FloatFilter {
@@ -138,6 +194,7 @@ type FloatFilter {
 	lte: Float
 	gt: Float
 	gte: Float
+	neq: Float
 }
 
 type IntFilter {
@@ -146,5 +203,18 @@ type IntFilter {
 	lte: Int
 	gt: Int
 	gte: Int
+	neq: Int
 }
 
+type BooleanFilter {
+	eq: Boolean
+}
+
+type DateFilter {
+	eq: Date
+	lt: Date
+	lte: Date
+	gt: Date
+	gte: Date
+	neq: Date
+}
